13c145181eee1bd89269aeb7cb509e01bede1643
ishrak99 <ishrak.khan@gmail.com>
2024-09-01 23:09:37 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import requests
+import urllib3
+import dnacentersdk
+import pyopenssl
+import python-dotenv
+import PyYAML
+import PyGithub
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
